931e8192f55d5b3b7d3d5fb50449be89f09983fd
serpatronics <101369949+serpatronics@users.noreply.github.com>
2022-08-26 20:42:39 -0300

    criado o index

---
+++ index.js
@@ -0,0 +1,6 @@
+var readlineSync = require('readline-sync')
+const PrimeiroNumero = readlineSync.question('Insira um numero : ')
+const SegundoNumero = readlineSync.question('Insira outro numero : ')
+console.log(PrimeiroNumero)
+console.log(SegundoNumero)
+
