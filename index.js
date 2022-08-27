1a997304f7bc4d671c6e7d43163613f822f832d7
serpatronics <101369949+serpatronics@users.noreply.github.com>
2022-08-27 16:03:35 -0300

    Criada a função somar

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const somar = require('./somar')
 var readlineSync = require('readline-sync')
 const PrimeiroNumero = readlineSync.question('Insira um numero : ')
 const SegundoNumero = readlineSync.question('Insira outro numero : ')
 const operador = ['Soma','Subtracao', 'Multiplicao','Divisao']
 const Operacao = readlineSync.keyInSelect(operador)
+
+
+
+
 console.log(PrimeiroNumero)
 console.log(SegundoNumero)
 console.log(Operacao)
+console.log(somar.soma(Number(PrimeiroNumero),Number(SegundoNumero)))
+
 
