ad1f9138b96ea55047e9f28f19f2c0dd09e6fdf5
serpatronics <101369949+serpatronics@users.noreply.github.com>
2022-08-26 21:33:12 -0300

    Criado o recebimento de parametros

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var readlineSync = require('readline-sync')
 const PrimeiroNumero = readlineSync.question('Insira um numero : ')
 const SegundoNumero = readlineSync.question('Insira outro numero : ')
+const operador = ['Soma','Subtracao', 'Multiplicao','Divisao']
+const Operacao = readlineSync.keyInSelect(operador)
 console.log(PrimeiroNumero)
 console.log(SegundoNumero)
+console.log(Operacao)
 
